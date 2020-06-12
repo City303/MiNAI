d11cea9076f53e6e14f782ade4fa46e61a1fbd2e
City303 <City303@users.noreply.github.com>
2020-06-12 00:03:01 -0500

    Work on py script

--- scripts/mina_batch.py
+++ scripts/mina_batch.py
@@ -1,5 +1,21 @@
-import pyimagej as ij
+import os
+import imagej
 import pandas as pd
 
-ij = imagej.init()
-ij.getVersion()
+ij = imagej.init('/home/mitocab/Downloads/Fiji.app')
+print(ij.getVersion())
+
+ROOT = '/home/mitocab/Documents/Box-05282020'
+FILE = 'BJ & SBG4-5/N3 5-2-19/No FCCP/SBG5/image 4/sbg5 p8 no fccp dish 6 r4 05-02-2019_cp_skel_1.tiff'
+MACRO_PATH = '/home/mitocab/Downloads/Fiji.app/macros/MiNA-py/mina_analysis.py'
+MACRO_EXT  = 'py'
+
+image = ij.io().open(os.path.join(ROOT,FILE))
+
+with open(MACRO_PATH) as macro:
+    macro_body = macro.read()
+    args = None
+    result = ij.py.run_script(MACRO_EXT, macro_body, args)
+
+print(result)
+print('Success')
