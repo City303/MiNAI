3e17ac328195ba07284652251e80b3de7c5ad501
City303 <City303@users.noreply.github.com>
2020-07-09 16:48:53 -0500

    Improved mina analysis notation 2

--- scripts/mina_batch.py
+++ scripts/mina_batch.py
@@ -7,36 +7,63 @@ import pandas as pd
 MACRO_PATH = '/home/mitocab/GitHub/MiNAI/fiji/mina_analysis.py'
 
 OUTPUT_ORDER = [
-        'image_title',
-        'thresholding_op',
-        'use_ridge_detection',
-        'high_contrast',
-        'low_contrast',
-        'line_width',
-        'min_line_length',
-        'mitochondrial_footprint',
-        'punctate_count',
-        'punctate_len_mean',
-        'punctate_len_med',
-        'punctate_len_stdevp',
-        'rod_count',
-        'rod_len_mean',
-        'rod_len_med',
-        'rod_len_stdevp',
-        'network_count',
-        'network_branch_count',
-        'network_len_mean',
-        # 'network_len_med',
-        # 'network_len_stdevp',
-        'branch_len_mean',
-        'branch_len_med',
-        'branch_len_stdevp',
-        'summed_branch_lens_mean',
-        'summed_branch_lens_med',
-        'summed_branch_lens_stdevp',
-        'network_branches_mean',
-        'network_branches_med',
-        'network_branches_stdevp',
+    'image_title',
+    'thresholding_op',
+    'use_ridge_detection',
+    'high_contrast',
+    'low_contrast',
+    'line_width',
+    'min_line_length',
+    'mitochondrial_footprint',
+    'punctate_count',
+    'rod_count',
+    'network_count',
+    'rod_len_mean',
+    'rod_len_med',
+    'rod_len_stdevp',
+    'network_branches',
+    'network_branches_mean',
+    'branch_len_mean',
+    'branch_len_med',
+    'branch_len_stdevp',
+    'summed_branch_lens_mean',
+    'summed_branch_lens_med',
+    'summed_branch_lens_stdevp',
+    'network_branches_mean',
+    'network_branches_med',
+    'network_branches_stdevp',
+]
+
+OUTPUT_FILTER = [
+    'image_title',
+    'mitochondrial_footprint',
+    'punctate_count',
+    'rod_count',
+    'network_count',
+    'rod_len_mean',
+    'rod_len_med',
+    'rod_len_stdevp',
+    'branch_len_mean',
+    'branch_len_med',
+    'branch_len_stdevp',
+    'network_branches',
+    'network_branches_mean',
+]
+
+OUTPUT_TITLES = [
+    'Image title',
+    'Mitochondrial footprint',
+    '# of punctates',
+    '# of rods',
+    '# of networks',
+    'Rod length mean',
+    'Rod length median',
+    'Rod length population stdev',
+    'Branch length mean',
+    'Branch length median',
+    'Branch length population stdev',
+    '# of network branches',
+    'Network branches length mean'
 ]
 
 
@@ -49,9 +76,8 @@ def main():
         mina_macro = f.read()
 
     mina_args = {
-        #'root_directory': '/home/mitocab/Documents/BatchScriptTest',
         'root_directory': '/home/mitocab/Documents/Box-05282020',
-        'regex_string': '.*_cp_skel_[0-9]*.*',
+        'regex_string'  : '.*_cp_skel_[0-9]*.*',
         'use_ridge_detection': False,
         'verbose': False,
     }
@@ -67,15 +93,17 @@ def main():
     for k, v in sorted(ij_out.items(), key=sort_by_order):
         # Format the items so that they are save-able into the CSV
         # (and to potentially re-cast them to their proper types)
-        v = v.strip('[]').split(', ')
-        for i in range(len(v)):
-            if v[i].startswith("u'") or v[i].startswith('u"'):
-                v[i] = v[i][2:]
-            v[i] = v[i].strip('\'\"')
-        
-        ij_out[k] = v
-        py_out[k] = v
-        print(k, ':\n', py_out[k])
+        if k in OUTPUT_FILTER:
+            idx   = OUTPUT_FITLER.index(k)
+            title = OUTPUT_TITLES[index]
+            v = v.strip('[]').split(', ')
+            for i in range(len(v)):
+                if v[i].startswith("u'") or v[i].startswith('u"'):
+                    v[i] = v[i][2:]
+                v[i] = v[i].strip('\'\"')
+            
+            py_out[title] = v
+            print(title, ':\n', py_out[title])
 
     df = pd.DataFrame.from_dict(py_out)
     df.to_csv('/home/mitocab/Documents/output.csv')
@@ -86,4 +114,11 @@ def main():
 Program execution starts here.
 '''
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 3:
+        print('Usage: python mina_batch.py [1] [2]')
+        print('[1]: root directory (i.e. "/home/mitocab/Documents/Box-05282020"')
+        print('[2]: regex to find skeleton files (i.e. ".*_cp_skel_[0-9]*.*"')
+
+    root_dir  = sys.argv[1]
+    regex_str = sys.argv[2]
+    main(root_dir, regex_str)
