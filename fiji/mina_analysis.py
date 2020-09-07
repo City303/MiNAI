53cf7f3605725a6aab900f0d175dff46b3e2a1d9
City303 <City303@users.noreply.github.com>
2020-09-07 12:56:22 -0500

    refined parameters

--- fiji/mina_analysis.py
+++ fiji/mina_analysis.py
@@ -158,7 +158,7 @@ def run():
         'rod_lens_stdevp',
 
         'network_num_branches_count',
-        'network_num_branches_mean',
+        'network_branch_lens_mean',
         'network_lens_mean',
         'network_lens_median',
         'network_lens_stdevp',
